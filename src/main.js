77b76beb2fc7fcc3b05343bf6f28f5889b0d2bf0
seoyoonjeong <yoonnjeong@easymedia.net>
2023-01-06 17:15:10 +0900

    store 등록

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from './store/store'
+// { store } 변수를 불러온다
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
+  // store: store
 }).$mount('#app')
