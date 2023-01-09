4985fa7e455aba48fe80f8b4d5dff8f1ee688705
seoyoonjeong <yoonnjeong@easymedia.net>
2023-01-09 16:51:40 +0900

    헬퍼 함수가 주는 간편함 예시본

---
+++ src/components/Demo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="root">
+    <p>{{ originalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+    <p>{{ tripplePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['originalPrice', 'doublePrice', 'tripplePrice']),
+
+    // originalPrice() {
+    //   return this.$store.getters.originalPrice;
+    // },
+    // doublePrice() {
+    //   return this.$store.getters.doublePrice;
+    // },
+    // tripplePrice() {
+    //   return this.$store.getters.tripplePrice;
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
