8f97dd76e56be7d283b2e01ab052412c4dbac6c8
seoyoonjeong <yoonnjeong@easymedia.net>
2023-01-09 17:28:08 +0900

    스토어 모듈화2

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,24 +1,24 @@
-const addOneItem = (state, todoItem) => {
-  const obj = {completed: false, item: todoItem};
-  localStorage.setItem(todoItem, JSON.stringify(obj)); 
-  state.todoItems.push(obj); 
-}
+// const addOneItem = (state, todoItem) => {
+//   const obj = {completed: false, item: todoItem};
+//   localStorage.setItem(todoItem, JSON.stringify(obj)); 
+//   state.todoItems.push(obj); 
+// }
 
-const removeOneItem = (state, payload) => {
-  localStorage.removeItem(payload.todoItem.item);
-  state.todoItems.splice(payload.index, 1);
-}
+// const removeOneItem = (state, payload) => {
+//   localStorage.removeItem(payload.todoItem.item);
+//   state.todoItems.splice(payload.index, 1);
+// }
 
-const toggleOneItem = (state, payload) => {
-  state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+// const toggleOneItem = (state, payload) => {
+//   state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
 
-  localStorage.removeItem(payload.todoItem.item);
-  localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-}
+//   localStorage.removeItem(payload.todoItem.item);
+//   localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+// }
 
-const clearAllItem = (state) => {
-  localStorage.clear();
-  state.todoItems = [];
-}
+// const clearAllItem = (state) => {
+//   localStorage.clear();
+//   state.todoItems = [];
+// }
 
-export { addOneItem, removeOneItem, toggleOneItem, clearAllItem }
+// export { addOneItem, removeOneItem, toggleOneItem, clearAllItem }
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './getters';
-import * as mutations from './mutations';
+
+import todoApp from './modules/todoApp'
+
 
 Vue.use(Vuex);
 
@@ -22,34 +23,8 @@ const storage = {
 }
 
 export const store = new Vuex.Store({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    }
-  },
-  //storedTodoItems를 선언해 state에 접근해서 todoItems을 가져온다. 
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = {completed: false, item: todoItem};
-      localStorage.setItem(todoItem, JSON.stringify(obj)); 
-      state.todoItems.push(obj); 
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-    },
-    clearAllItem(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    }
+  modules: {
+    // todoApp: todoApp
+    todoApp
   }
 });
